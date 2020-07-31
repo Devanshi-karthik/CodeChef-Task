1ea23ad3d01dd33bd5825dbbc3070d06505bc11f
Devanshi-karthik <53863556+Devanshi-karthik@users.noreply.github.com>
2020-07-31 21:44:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,19 +13,13 @@ const navSlide = () => {
         link.style.animation = ''
       }else{
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-
       }
     });
 
     //burger animation
     burger.classList.toggle('toggle');
 
-
   });
-
-
-
-
 }
 
 navSlide();
